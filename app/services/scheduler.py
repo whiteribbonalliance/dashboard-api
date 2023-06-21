f653a02eb5766e80c6a915f5b346e613e490c798
Zairon Jacobs <zaijacobs@gmail.com>
2023-06-21 07:35:35 +0200

    add log for scheduler

--- app/services/scheduler.py
+++ app/services/scheduler.py
@@ -1,21 +1,33 @@
 """
-Scheduler contains a task that runs every 12th hour to load data from BigQuery
+Scheduler contains a task that runs every 12th hour
 """
+
+import logging
 
 from fastapi.concurrency import run_in_threadpool
 from rocketry import Rocketry
 from rocketry.conds import cron
 
+from app.logginglib import init_custom_logger
 from app.utils import data_loader
 
 app = Rocketry(executation="async")
+
+logger = logging.getLogger(__name__)
+init_custom_logger(logger)
 
 
 @app.task(cron("0 */12 * * *"))
 async def do_every_12th_hour():
     """
     Load data from BigQuery
+    Load coordinates
+
     Runs at minute 0 past every 12th hour
     """
 
-    await run_in_threadpool(data_loader.load_data)
+    try:
+        await run_in_threadpool(data_loader.load_data)
+        await run_in_threadpool(data_loader.load_coordinates)
+    except (Exception,) as e:
+        logger.error(f"Error while trying to load data: {str(e)}")
