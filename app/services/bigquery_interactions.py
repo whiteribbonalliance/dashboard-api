2ec4752ede38ff2e4cb987eb36ce849fa1b9261b
Thomas Wood <woodthom@gmail.com>
2023-07-28 08:45:23 +0100

    Update bigquery_interactions.py

--- app/services/bigquery_interactions.py
+++ app/services/bigquery_interactions.py
@@ -68,7 +68,7 @@ def get_campaign_df_from_bigquery(campaign_code: CampaignCode) -> DataFrame:
         response_lemmatized_text as lemmatized,
         respondent_region_name as region,
         coalesce(cast(respondent_age as string),respondent_age_bucket) as age,
-        REGEXP_REPLACE(REGEXP_REPLACE(INITCAP(respondent_gender), 'Twospirit', 'Two spirit'), 'Unspecified', 'Prefer not to say') as gender,
+        REGEXP_REPLACE(REGEXP_REPLACE(INITCAP(respondent_gender), 'Twospirit', 'Two Spirit'), 'Unspecified', 'Prefer Not To Say') as gender,
         JSON_VALUE(respondent_additional_fields.profession) as profession,
         FROM deft-stratum-290216.{table_name}
         WHERE campaign = '{campaign_code.value}'
