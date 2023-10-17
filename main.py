2a61fae37cc75871a3752ae8c736cd837b9786e0
Zairon Jacobs <zaijacobs@gmail.com>
2023-10-17 15:47:27 +0200

    increase workers

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def main():
             host=settings.SERVER_HOST,
             port=settings.PORT,
             reload=settings.RELOAD,
-            workers=1,
+            workers=2,
             loop="asyncio",
         )
     )
