9550a39f17179fbb04022abfaee1cd66271ace43
Zairon Jacobs <zaijacobs@gmail.com>
2023-11-22 07:23:51 +0100

    update cors

--- app/core/settings.py
+++ app/core/settings.py
@@ -49,6 +49,7 @@ class ProdSettings(Settings):
             "https://whatyoungpeoplewant.whiteribbonalliance.org",
             "https://midwivesvoices.whiteribbonalliance.org",
             "https://admin.whiteribbonalliance.org",
+            "https://pmnch-front.azurewebsites.net"
         ],
         "allow_credentials": True,
         "allow_methods": ["GET", "POST", "OPTIONS", "HEAD"],
