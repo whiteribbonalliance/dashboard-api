764d2154eaf1abbd5b93e378dea1f5d0b1db6168
Zairon Jacobs <zaijacobs@gmail.com>
2023-12-21 16:51:03 +0100

    add connection_timeout to download_blob func

--- app/services/azure_blob_storage_interactions.py
+++ app/services/azure_blob_storage_interactions.py
@@ -133,7 +133,9 @@ def get_blob(container_name: str, blob_name: str) -> StorageStreamDownloader[byt
     container_client = get_container_client(container_name=container_name)
 
     try:
-        return container_client.download_blob(blob=blob_name, max_concurrency=3)
+        return container_client.download_blob(
+            blob=blob_name, max_concurrency=3, connection_timeout=3600
+        )
     except (Exception,) as e:
         raise Exception(f"Could not get blob: {str(e)}.")
 
