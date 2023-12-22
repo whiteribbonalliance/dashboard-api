3217ecdaefe7cad82d990229c31c729004105e98
Zairon Jacobs <zaijacobs@gmail.com>
2023-12-22 11:17:24 +0100

    fix configurations translations

--- app/api/v1/endpoints/campaign_configurations.py
+++ app/api/v1/endpoints/campaign_configurations.py
@@ -24,12 +24,15 @@ SOFTWARE.
 """
 
 import copy
+import logging
+
 from fastapi import APIRouter, status, Depends, Request
 
 from app.api import dependencies
 from app.core.settings import get_settings
 from app.helpers.campaigns_config_loader import CAMPAIGNS_CONFIG
 from app.http_exceptions import ResourceNotFoundHTTPException
+from app.logginglib import init_custom_logger
 from app.schemas.campaign_config import CampaignConfigResponse, CampaignConfigInternal
 from app.services.api_cache import ApiCache
 from app.services.translator import Translator
@@ -39,6 +42,8 @@ settings = get_settings()
 router = APIRouter(prefix="/configurations")
 
 api_cache = ApiCache()
+logger = logging.getLogger(__name__)
+init_custom_logger(logger)
 
 
 @router.get(
@@ -78,37 +83,44 @@ def read_campaigns_configurations(
 
         # Translate
         if settings.TRANSLATIONS_ENABLED:
-            translator = Translator(
-                target_language=language, cloud_service=settings.CLOUD_SERVICE
-            )
-            e = translator.extract_text
-            t = translator.translate_text
+            try:
+                translator = Translator(
+                    target_language=language, cloud_service=settings.CLOUD_SERVICE
+                )
+                e = translator.extract_text
+                t = translator.translate_text
 
-            for configuration in configurations:
                 # Extract
-                configuration.campaign_title = e(configuration.campaign_title)
-                configuration.campaign_subtext = e(configuration.campaign_subtext)
-                configuration.respondent_noun_singular = e(
-                    configuration.respondent_noun_singular
-                )
-                configuration.site_title = e(configuration.site_description)
-                configuration.site_description = e(configuration.site_description)
-                configuration.respondent_noun_plural = e(
-                    configuration.respondent_noun_plural
-                )
+                for configuration in configurations:
+                    configuration.campaign_title = e(configuration.campaign_title)
+                    configuration.campaign_subtext = e(configuration.campaign_subtext)
+                    configuration.respondent_noun_singular = e(
+                        configuration.respondent_noun_singular
+                    )
+                    configuration.site_title = e(configuration.site_description)
+                    configuration.site_description = e(configuration.site_description)
+                    configuration.respondent_noun_plural = e(
+                        configuration.respondent_noun_plural
+                    )
 
                 translator.translate_extracted_texts()
 
-                # Translate
-                configuration.campaign_title = t(configuration.campaign_title)
-                configuration.campaign_subtext = t(configuration.campaign_subtext)
-                configuration.respondent_noun_singular = t(
-                    configuration.respondent_noun_singular
-                )
-                configuration.site_title = t(configuration.site_description)
-                configuration.site_description = t(configuration.site_description)
-                configuration.respondent_noun_plural = t(
-                    configuration.respondent_noun_plural
+                # Apply translations
+                for configuration in configurations:
+                    configuration.campaign_title = t(configuration.campaign_title)
+                    configuration.campaign_subtext = t(configuration.campaign_subtext)
+                    configuration.respondent_noun_singular = t(
+                        configuration.respondent_noun_singular
+                    )
+                    configuration.site_title = t(configuration.site_description)
+                    configuration.site_description = t(configuration.site_description)
+                    configuration.respondent_noun_plural = t(
+                        configuration.respondent_noun_plural
+                    )
+
+            except (Exception,) as e:
+                logger.warning(
+                    f"An error occurred during translation of campaign config: {str(e)}"
                 )
 
         return configurations
@@ -121,6 +133,7 @@ def read_campaigns_configurations(
     response_model=CampaignConfigResponse,
     status_code=status.HTTP_200_OK,
 )
+@api_cache.cache_response
 def read_campaign_configuration(
     _request: Request,
     campaign_code: str = Depends(dependencies.campaign_code_exists_check),
@@ -135,37 +148,42 @@ def read_campaign_configuration(
         configuration = copy.deepcopy(configuration)
         # Translate
         if settings.TRANSLATIONS_ENABLED:
-            translator = Translator(
-                target_language=language, cloud_service=settings.CLOUD_SERVICE
-            )
-            e = translator.extract_text
-            t = translator.translate_text
+            try:
+                translator = Translator(
+                    target_language=language, cloud_service=settings.CLOUD_SERVICE
+                )
+                e = translator.extract_text
+                t = translator.translate_text
 
-            # Extract
-            configuration.campaign_title = e(configuration.campaign_title)
-            configuration.campaign_subtext = e(configuration.campaign_subtext)
-            configuration.respondent_noun_singular = e(
-                configuration.respondent_noun_singular
-            )
-            configuration.site_title = e(configuration.site_description)
-            configuration.site_description = e(configuration.site_description)
-            configuration.respondent_noun_plural = e(
-                configuration.respondent_noun_plural
-            )
+                # Extract
+                configuration.campaign_title = e(configuration.campaign_title)
+                configuration.campaign_subtext = e(configuration.campaign_subtext)
+                configuration.respondent_noun_singular = e(
+                    configuration.respondent_noun_singular
+                )
+                configuration.site_title = e(configuration.site_description)
+                configuration.site_description = e(configuration.site_description)
+                configuration.respondent_noun_plural = e(
+                    configuration.respondent_noun_plural
+                )
 
-            translator.translate_extracted_texts()
+                translator.translate_extracted_texts()
 
-            # Apply translations
-            configuration.campaign_title = t(configuration.campaign_title)
-            configuration.campaign_subtext = t(configuration.campaign_subtext)
-            configuration.respondent_noun_singular = t(
-                configuration.respondent_noun_singular
-            )
-            configuration.site_title = t(configuration.site_description)
-            configuration.site_description = t(configuration.site_description)
-            configuration.respondent_noun_plural = t(
-                configuration.respondent_noun_plural
-            )
+                # Apply translations
+                configuration.campaign_title = t(configuration.campaign_title)
+                configuration.campaign_subtext = t(configuration.campaign_subtext)
+                configuration.respondent_noun_singular = t(
+                    configuration.respondent_noun_singular
+                )
+                configuration.site_title = t(configuration.site_description)
+                configuration.site_description = t(configuration.site_description)
+                configuration.respondent_noun_plural = t(
+                    configuration.respondent_noun_plural
+                )
+            except (Exception,) as e:
+                logger.warning(
+                    f"An error occurred during translation of campaign config: {str(e)}"
+                )
 
         return configuration
 
