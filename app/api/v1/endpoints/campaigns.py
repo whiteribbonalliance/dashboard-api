5ec963af1658fac43ea9a5e3cd41373bb08056c4
Zairon Jacobs <zaijacobs@gmail.com>
2024-02-22 13:28:31 +0100

    allow other campaigns at endpoint func download_campaign_public_data

--- app/api/v1/endpoints/campaigns.py
+++ app/api/v1/endpoints/campaigns.py
@@ -161,15 +161,6 @@ def download_campaign_public_data(
     filter_1 = campaign_req.filter_1
     filter_2 = campaign_req.filter_2
 
-    # Only allow legacy campaign healthwellbeing and dataexchange
-    if (
-        campaign_code != LegacyCampaignCode.healthwellbeing.value
-        and campaign_code != LegacyCampaignCode.dataexchange.value
-    ):
-        raise http_exceptions.UnauthorizedHTTPException(
-            "Reading campaign data not allowed."
-        )
-
     # Service
     campaign_service = CampaignService(
         campaign_code=campaign_code,
